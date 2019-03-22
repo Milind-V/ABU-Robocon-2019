7faebdb682f28a17f8d778a8165a1590fc684e55
mushonnip <mushonnipabu@gmail.com>
2019-03-22 22:20:08 +0700

    Old

--- app.ino
+++ app.ino
@@ -269,48 +269,48 @@ void setup()
     Serial.begin(57600);
     delay(300);
     error = ps2x.config_gamepad(PS2_CLK, PS2_CMD, PS2_SEL, PS2_DAT, pressures, rumble);
-    if (error == 0)
-    {
-        Serial.print("Found Controller, configured successful ");
-        Serial.print("pressures = ");
-        if (pressures)
-            Serial.println("true ");
-        else
-            Serial.println("false");
-        Serial.print("rumble = ");
-        if (rumble)
-            Serial.println("true)");
-        else
-            Serial.println("false");
-        Serial.println("Try out all the buttons, X will vibrate the controller, faster as you press harder;");
-        Serial.println("holding L1 or R1 will print out the analog stick values.");
-        Serial.println("Note: Go to www.billporter.info for updates and to report bugs.");
-    }
-    else if (error == 1)
-        Serial.println("No controller found, check wiring, see readme.txt to enable debug. visit www.billporter.info for troubleshooting tips");
+    // if (error == 0)
+    // {
+    //     Serial.print("Found Controller, configured successful ");
+    //     Serial.print("pressures = ");
+    //     if (pressures)
+    //         Serial.println("true ");
+    //     else
+    //         Serial.println("false");
+    //     Serial.print("rumble = ");
+    //     if (rumble)
+    //         Serial.println("true)");
+    //     else
+    //         Serial.println("false");
+    //     Serial.println("Try out all the buttons, X will vibrate the controller, faster as you press harder;");
+    //     Serial.println("holding L1 or R1 will print out the analog stick values.");
+    //     Serial.println("Note: Go to www.billporter.info for updates and to report bugs.");
+    // }
+    // else if (error == 1)
+    //     Serial.println("No controller found, check wiring, see readme.txt to enable debug. visit www.billporter.info for troubleshooting tips");
 
-    else if (error == 2)
-        Serial.println("Controller found but not accepting commands. see readme.txt to enable debug. Visit www.billporter.info for troubleshooting tips");
+    // else if (error == 2)
+    //     Serial.println("Controller found but not accepting commands. see readme.txt to enable debug. Visit www.billporter.info for troubleshooting tips");
 
-    else if (error == 3)
-        Serial.println("Controller refusing to enter Pressures mode, may not support it. ");
+    // else if (error == 3)
+    //     Serial.println("Controller refusing to enter Pressures mode, may not support it. ");
 
-    type = ps2x.readType();
-    switch (type)
-    {
-    case 0:
-        Serial.print("Unknown Controller type found ");
-        break;
-    case 1:
-        Serial.print("DualShock Controller found ");
-        break;
-    case 2:
-        Serial.print("GuitarHero Controller found ");
-        break;
-    case 3:
-        Serial.print("Wireless Sony DualShock Controller found ");
-        break;
-    }
+    // type = ps2x.readType();
+    // switch (type)
+    // {
+    // case 0:
+    //     Serial.print("Unknown Controller type found ");
+    //     break;
+    // case 1:
+    //     Serial.print("DualShock Controller found ");
+    //     break;
+    // case 2:
+    //     Serial.print("GuitarHero Controller found ");
+    //     break;
+    // case 3:
+    //     Serial.print("Wireless Sony DualShock Controller found ");
+    //     break;
+    // }
 }
 
 void loop()
