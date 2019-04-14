d3098da1fbfbffb1435ddf755456e2324ab72812
mushonnip <mushonnipabu@gmail.com>
2019-04-14 16:06:12 +0700

    fix some mistake

--- app.ino
+++ app.ino
@@ -3,14 +3,14 @@
 #define relay1 36
 #define relay2 38
 
-#define pwm4a 3
-#define pwm4b 2
+#define pwm4a 5
+#define pwm4b 4
 
 #define pwm2a 9
 #define pwm2b 8
 
-#define pwm1a 5
-#define pwm1b 4
+#define pwm1a 11
+#define pwm1b 10
 
 #define pwm3a 6
 #define pwm3b 7
@@ -20,7 +20,8 @@
 #define PS2_SEL 33
 #define PS2_CLK 31
 
-#define maxSpeed 90
+#define maxSpeed 100
+#define halfSpeed 50
 
 #define pressures false
 #define rumble false
@@ -71,9 +72,11 @@ void motor(int p1a, int p1b, int p2a, int p2b, int p3a, int p3b, int p4a, int p4
 
 void setup()
 {
+    //Relay Pnumatik
     pinMode(relay1, OUTPUT);
     pinMode(relay2, OUTPUT);
 
+    //Pin Motor
     pinMode(pwm1a, OUTPUT);
     pinMode(pwm1b, OUTPUT);
     pinMode(pwm2a, OUTPUT);
@@ -89,6 +92,7 @@ void setup()
 
 void loop()
 {
+
     ps2x.read_gamepad();
     motor(0, 0, 0, 0, 0, 0, 0, 0);
 
@@ -100,7 +104,6 @@ void loop()
     {
         digitalWrite(relay1, HIGH);
     }
-
     if (ps2x.ButtonPressed(PSB_CIRCLE))
     {
         digitalWrite(relay2, LOW);
@@ -110,46 +113,28 @@ void loop()
         digitalWrite(relay2, HIGH);
     }
 
-    //Tombol SELECT
-    if (ps2x.ButtonPressed(PSB_SELECT))
+        if (ps2x.Button(PSB_CROSS) || ps2x.Button(PSB_PAD_UP))
     {
-        autoMode = true;
+        motor(0, 0, 0, maxSpeed, 0, 0, maxSpeed, 0); //MAJU
     }
-
-    //MAJU
-    if (ps2x.Button(PSB_CROSS) || ps2x.Button(PSB_PAD_UP))
-    {
-        motor(0, 0, 0, maxSpeed, 0, 0, maxSpeed, 0);
-    }
-
-    //MUNDUR
     if (ps2x.Button(PSB_SQUARE) || ps2x.Button(PSB_PAD_DOWN))
     {
-        motor(0, 0, 50, 0, 0, 0, 0, 50);
+        motor(0, 0, halfSpeed, 0, 0, 0, 0, halfSpeed); //MUNDUR
     }
-
-    ///KANAN
     if (ps2x.Button(PSB_PAD_RIGHT))
     {
-        motor(maxSpeed, 0, 0, 0, maxSpeed, 0, 0, 0);
+        motor(maxSpeed, 0, 0, 0, maxSpeed, 0, 0, 0); ///KANAN
     }
-
-    //KIRI
     if (ps2x.Button(PSB_PAD_LEFT))
     {
-        motor(0, maxSpeed, 0, 0, 0, maxSpeed, 0, 0);
+        motor(0, maxSpeed, 0, 0, 0, maxSpeed, 0, 0); //KIRI
     }
-
-    //PUTAR KANAN
+    if (ps2x.Button(PSB_L1))
+    {
+        motor(0, halfSpeed, 0, halfSpeed, halfSpeed, 0, 0, halfSpeed); //PUTAR KANAN
+    }
     if (ps2x.Button(PSB_R1))
     {
-        motor(0, 50, 0, 50, 50, 0, 0, 50);
-    }
-
-    //PUTAR KIRI
-    if (ps2x.Button(PSB_L1))
-
-    {
-        motor(50, 0, 50, 0, 0, 50, 50, 0);
+        motor(halfSpeed, 0, halfSpeed, 0, 0, halfSpeed, halfSpeed, 0); //PUTAR KIRI
     }
 }
